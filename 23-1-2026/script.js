4bd25f95b548e132eee88bc957e96d358080e817
hasnainasif0761 <hasnainasif07610761@gmail.com>
2026-01-25 00:48:33 +0500

    'first-Change'

---
+++ 23-1-2026/script.js
@@ -0,0 +1,41 @@
+console.log("Browser Control Events");
+
+// console.log(window.location.href)
+
+// console.log(window.location.reload())
+
+// function timeout (){
+//     console.log('3 Second Bad');   // High Order Function 
+// }
+
+// setTimeout(timeout,3000)
+
+
+// window.setInterval(()=>{
+//     console.log("Running.....")
+// },1000)
+
+
+// window.open();
+
+
+// console.log(window.innerWidth);
+// console.log(window.innerHeight);
+
+
+
+// // var openPage = window.open();
+
+// var content = "<h1>Capuchin monkey</h1><img src= 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQZYHDPoyX1xSBOzbzFTh7SJ69YPMxX8MD84Q&s'><p>The word capuchin derives from agroup of friars<br>named the Order of Friars Minor Capuchin who wear<br>brownrobes with large hoods covering their heads.</p>";
+
+// openPage.document.writeln(content)
+
+// window.open('about.html','win1',"width=500,height=500");
+
+
+
+
+
+
+
+
