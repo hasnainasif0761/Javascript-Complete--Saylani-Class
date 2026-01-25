f714988637c1c93f1e375fd01c4acc4e23baaa99
hasnainasif0761 <hasnainasif07610761@gmail.com>
2026-01-25 11:42:55 +0500

    'Hight-and-Callback-Func'

--- 23-1-2026/script.js
+++ 23-1-2026/script.js
@@ -30,7 +30,87 @@ console.log("Browser Control Events");
 
 // openPage.document.writeln(content)
 
-// window.open('about.html','win1',"width=500,height=500");
+    // window.open('about.html','win1',"width=500,height=500");
+
+
+
+// function Func(a,b) {
+//     return a + b          // Function Declaration
+// }
+// console.log(Func(2,2))
+
+
+
+
+// function Func(fname,lname,calback) {
+//     console.log(fname,lname);  // High Order Function and Callback Function
+//     calback()          
+// }
+
+// function calback(){
+//     console.log('Hello I am Callback Function');
+// }
+
+// Func('ibrahim','khan',calback)
+
+
+
+// const arr = [1,2,3,4,5];
+
+// const result = arr.map(function(num){  // High Order Function OR Callback Function
+//     return num * num
+// }) 
+
+// console.log(arr);
+// console.log(result);
+
+// var products = [
+//   { name: 'Watch', price: 3000 },
+//   { name: 'Mobile', price: 50000 },
+//   { name: 'Headphone', price: 2000 }
+// ];
+
+// const cheapProduct = products.filter(function(p){
+//     return p.price < 5000
+// })
+
+// console.table(products)
+// console.table(cheapProduct)
+
+
+// let age = [12,18,20,15, 30];
+
+// let adult = age.filter(function(aged){
+//     return aged >= 18
+// });
+
+// if(adult){
+//     console.log('Your are eligible for this Vote')
+// }else{
+//     console.log('Your are eligible for this Vote')
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
