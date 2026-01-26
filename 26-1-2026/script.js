c09c21acb73cb257f656ad733aa4254636268fd7
hasnainasif0761 <hasnainasif07610761@gmail.com>
2026-01-27 00:44:55 +0500

    '2-Updation'

---
+++ 26-1-2026/script.js
@@ -0,0 +1,112 @@
+console.log('Hello World');
+
+// function innerfunction(){
+//     console.log("khan")
+// }
+
+// function main(fname,lname){
+//     console.log(`Hi ${fname} `);
+//     lname()
+// }
+
+// main("Huzaifa",innerfunction);
+
+
+
+// function calculate (a,b){
+//     return multi(a,b);
+// }
+
+// function sum(e,f){
+//     return  e + f
+// }
+// function sub(e,f){
+//     return  e - f
+// }
+// function multi(e,f){
+//     return  e * f
+// }
+
+// console.log(calculate(2,5))
+
+
+
+// var arr = [1,2,3,4,5];
+
+// const mapArr = arr.map(function(items){
+//     return items*2
+// }) 
+
+// console.log(mapArr)
+
+
+// var arr = [1,2,3,4,5];
+
+// var filterArr = arr.filter(items=> items % 2 ==0 );
+
+// console.log(filterArr)
+
+
+// var arr = [1,2,3,4,5];
+
+//                      //acc => accumulator then total current Value   
+// var reduceArr = arr.reduce((acc,total)=>acc+total); // default 0
+
+// console.log(reduceArr)
+
+
+// var arr = [1,2,3,4,5];
+// var arr = [-2,-1,-1];
+
+// var someArr = arr.some(function(num){
+//     return num > 0
+// })
+
+// console.log(someArr);
+
+
+// var arr = [1,2,3,4,5];
+
+// let findArr = arr.find((num)=> num > 0);
+
+// console.log(findArr)
+
+
+
+// const user = {
+//   plan: "free",          // "free" | "pro" | "enterprise"
+//   projectsUsed: 2,
+//   projectsLimit: 5,
+//   storageUsed: 1.2,
+//   storageLimit: 5
+// };
+
+
+
+
+// const allFeatures = [
+//   { name: "5 Projects",           pro: false },
+//   { name: "Unlimited Projects",   pro: true  },
+//   { name: "5 GB Storage",         pro: false },
+//   { name: "50 GB Storage",        pro: true  },
+//   { name: "Basic Analytics",      pro: false },
+//   { name: "Advanced Analytics",   pro: true  },
+//   { name: "Community Support",    pro: false },
+//   { name: "Priority Support",     pro: true  },
+//   { name: "Custom Domain",        pro: true  },
+//   { name: "Remove Watermark",     pro: true  },
+//   { name: "API Access",           pro: true  }
+// ];
+
+// var filterPlane = allFeatures.forEach((feat)=>{
+//    let available = (user.plan === "pro") || !feat.pro
+//    console.log(available)
+// })
+
+
+
+
+
+
+
+
