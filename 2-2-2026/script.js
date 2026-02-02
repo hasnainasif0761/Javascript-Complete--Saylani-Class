89d2b6d72335787763ed0ab5947274680702d3ed
hasnainasif0761 <hasnainasif07610761@gmail.com>
2026-02-03 00:58:44 +0500

    'Second-Updation'

---
+++ 2-2-2026/script.js
@@ -0,0 +1,33 @@
+// const person = {
+//     firstName: "Huzaifa",
+//     lastName: "Asif",
+//     fullName: function (){          // Method
+//         return console.log(`Hello ${this.firstName}`);
+//     }
+// }
+
+// person.fullName();
+
+
+
+// function myFunction(){
+//     return this;
+// }
+
+// console.log(myFunction())
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ 30-1-2026/script.js
@@ -0,0 +1,24 @@
+// function myfunction(){
+//     let a = 4;    //local Scope Variable
+//     return a * a;
+// }
+// console.log(myfunction())
+
+// let a = 2;    // global Scope
+// function checkNum(){
+//     console.log(a);
+// }
+// checkNum()
+
+
+// function checkVar(){
+//     uname = "Ali";   //Global Scop Variable
+//     return uname;
+// }
+// console.log(checkVar())
+// console.log(uname)
+
+
+
+
+
