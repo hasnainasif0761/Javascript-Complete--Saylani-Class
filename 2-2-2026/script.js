172984355abac9aba13dd3f9fc1d79ea09d2e96a
hasnainasif0761 <hasnainasif07610761@gmail.com>
2026-02-03 01:43:21 +0500

    '6-Updation'

--- 2-2-2026/script.js
+++ 2-2-2026/script.js
@@ -1,36 +1,62 @@
-// const person = {
-//     firstName: "Huzaifa",
-//     lastName: "Asif",
-//     fullName: function (){          // Method
-//         return console.log(`Hello ${this.firstName}`);
+// // const person = {
+// //     firstName: "Huzaifa",
+// //     lastName: "Asif",
+// //     fullName: function (){          // Method
+// //         return console.log(`Hello ${this.firstName}`);
+// //     }
+// // }
+
+// // person.fullName();
+
+
+
+// // function myFunction(){
+// //     return this;
+// // }
+
+// // console.log(myFunction())
+
+
+
+// // Use Strick Mode For not binding your global object
+// // "use strict"
+// // function myFunction(){
+// //     return this;
+// // }
+
+// // console.log(myFunction())
+
+
+// // let person1 = {name:"Huzaifa"};
+// // let person2 = {name:"Ali"}
+
+// // function greet(greeting){
+// //     console.log(`${greeting} , ${this.name}`)
+// // }
+
+// // greet.call(person1,'Hello');
+
+
+// let person = {
+//     function: function(){
+//         return this.name + " "+ this.lname
 //     }
 // }
 
-// person.fullName();
-
-
-
-// function myFunction(){
-//     return this;
+// let person1 = {
+//     name:"Ali",
+//     lname:"Shakeel"
 // }
 
-// console.log(myFunction())
-
-
-
-// Use Strick Mode For not binding your global object
-// "use strict"
-// function myFunction(){
-//     return this;
+// let person2 = {
+//     name:"Razi ullah",
+//     lname:"Asif"
 // }
 
-// console.log(myFunction())
 
+// var result = person.function.call(person1)
+// console.log(result)
 
-
-hello = function(){
-    document.
-}
 
 
 
