434a228d005bc16bb0ade624cab27740ec47b9b2
hasnainasif0761 <hasnainasif07610761@gmail.com>
2026-02-07 01:24:24 +0500

    '6-2-2026'

---
+++ 6-2-2026/script.js
@@ -0,0 +1,39 @@
+console.log("Js is running");
+
+// IIFE  Stand for Immediately Invoked Function Expression
+
+// let outside = 'outside-funct'
+// function test(){
+//     let uname = 'ali';
+//     let outside = 'Inside-funct'
+//     console.log(outside)
+// }
+// test()
+
+// (function(){
+//     console.log('inside iife') ==> IIFE Function
+// })()
+
+
+// (()=>{
+//     uname = 'Huzaifa';
+//     console.log('Your name is '+uname)  // output is Huzaifa
+// })()
+
+// console.log(uname)  // output is Huzaifa
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
