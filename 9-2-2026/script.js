b3828625ad9159e72de8057f3c7204fb8d3c2007
hasnainasif0761 <hasnainasif07610761@gmail.com>
2026-02-11 00:09:14 +0500

    '9-2-2026-2-change'

---
+++ 9-2-2026/script.js
@@ -0,0 +1,45 @@
+// console.log(window.localStorage);
+
+// var uname = 'Ali';
+
+// localStorage.setItem('uname',uname);
+// let userName = localStorage.getItem('uname');
+
+// console.log("uname is "+userName)
+
+// let age = 10;
+
+// localStorage.setItem('age',age);
+// let getAge = localStorage.getItem('age');
+
+// document.writeln(getAge)
+// console.log(getAge)
+
+
+// var obj = {
+//     uname:'Ali',
+//     age:20
+// }
+
+// let convertObjInString = JSON.stringify(obj);
+
+// localStorage.setItem('object',convertObjInString);
+
+// let getObj = localStorage.getItem('object');
+
+// let convertStrInObj = JSON.parse(getObj)
+
+
+// console.log(convertStrInObj);
+
+
+
+// localStorage.removeItem('uname');
+// localStorage.clear()
+
+
+
+
+
+
+
