2672fc3cdb8c5d82ba391bdbf135036ad64af1ae
hasnainasif0761 <hasnainasif07610761@gmail.com>
2026-02-13 21:52:40 +0500

    '13-2-2026'

---
+++ 13-2-2026/script.js
@@ -0,0 +1,96 @@
+// function 
+// Callback functions
+
+// const func = function myFunc(){
+//     console.log('Inside Function');  // This is called Function Expression
+// }
+// func()
+
+
+// // Simple Function Making
+// function simpleFunc(){
+//     console.log('Simple Function Design');
+// }
+// simpleFunc()
+
+// Calculation Function 
+// function calculate(a,b){
+//     return a + b;
+// }
+
+// console.log(calculate(2,3));
+
+
+// es6
+// Arrow Function UseCase
+// const arrowFunc = (a,b) => a + b;
+
+// console.log(arrowFunc(5,10));
+
+// var num1 = prompt('Enter your first Number');
+// var num2 = prompt('Enter your second number');
+
+// // Convert prompt value into integer
+// num1 = parseInt(num1);
+// num2 = parseInt(num2);
+
+// function test(a,b){
+//     console.log(a+b)
+// }
+// test(num1,num2);
+
+
+// function main(a,b,sum){
+//     return sum(a,b)
+// }
+// console.log(main(10,40,sum))  // is tarha ka function ko callback function kahta ha
+
+// function sum(c,d){
+//     return c + d;
+// }
+
+// let obj = {
+//     a:5,
+//     b:3
+// }
+
+// let {a,b} = obj;  // is ko destructuring kahta ha na 
+
+// function checkVal(a,b){
+//     return a + b
+// }
+
+// console.log(checkVal(a,b))
+
+
+// function delay(){
+//     console.log("I will Console Lately");
+// }
+
+
+// setTimeout(delay,1000)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
