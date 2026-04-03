f26697d9593cd1a78ae210958bfaad4cc7e61259
hasnainasif0761 <hasnainasif07610761@gmail.com>
2026-04-04 01:24:22 +0500

    'complete-typescript'

--- TypeScript/typescript.js
+++ TypeScript/typescript.js
@@ -1,20 +1,20 @@
 "use strict";
-var sname = "ibrahim";
-console.log(sname);
-var sage = 18;
-console.log(sage);
+// var sname:string = "ibrahim";
+// console.log(sname);
+// var sage:number = 18;
+// console.log(sage)
 // let isactive:boolean = true;
 // console.log(isactive)
 //1) explicit type
 // explicit type ma type khud sa define karna hute ha
 // Example:
-var add = 2; // type hard coded define is called explicit type
-console.log(add);
+// var add:number = 2; // type hard coded define is called explicit type
+// console.log(add)
 // 2) implicit Type
 // Implicit type kya hute ha ka javascript khud sa type define kar deta ha example as javacroipt
 // Matlab
-var flag = true; // type auto difine is called implicit type
-console.log(flag);
+// var flag = true;  // type auto difine is called implicit type
+// console.log(flag)
 // 3) Speacial Type
 // Special type wo hute ha ja ham typescript per depend kar deta ha ka agar hamare type number huwe to 
 // wo auto matic number or agar string to auto matic string
@@ -27,6 +27,9 @@ console.log(flag);
 // Example:
 // var arr:[number,string,boolean] = [1,'ali',true]
 // console.log(arr)
+// var arr1: readonly [number,string,boolean] = [1,'bilal',true];
+// arr1.push('khan')
+// console.log(arr1)
 // 2) Enum 
 // Enum Set Speacific keys in constant
 // Two type of Enum
@@ -38,11 +41,18 @@ console.log(flag);
 //     sage,
 //     sroll
 // }
-// String
+// 2) String 
 var obj2;
 (function (obj2) {
-    obj2["sname"] = "name";
+    obj2["sname"] = "byd";
     obj2["sage"] = "age";
     obj2["sroll"] = "roll";
 })(obj2 || (obj2 = {}));
-console.log(obj2);
+console.log(obj2.sname);
+const bike = {
+    bname: 'unique',
+    bmodal: 2025,
+    bactive: true,
+    bnumber: 123
+};
+console.log("bilke=>", bike);
--- TypeScript/typescript.ts
+++ TypeScript/typescript.ts
@@ -1,8 +1,8 @@
-var sname:string = "ibrahim";
-console.log(sname);
+// var sname:string = "ibrahim";
+// console.log(sname);
 
-var sage:number = 18;
-console.log(sage)
+// var sage:number = 18;
+// console.log(sage)
 
 // let isactive:boolean = true;
 // console.log(isactive)
@@ -10,14 +10,14 @@ console.log(sage)
 //1) explicit type
 // explicit type ma type khud sa define karna hute ha
 // Example:
-var add:number = 2; // type hard coded define is called explicit type
-console.log(add)
+// var add:number = 2; // type hard coded define is called explicit type
+// console.log(add)
 
 // 2) implicit Type
 // Implicit type kya hute ha ka javascript khud sa type define kar deta ha example as javacroipt
 // Matlab
-var flag = true;  // type auto difine is called implicit type
-console.log(flag)
+// var flag = true;  // type auto difine is called implicit type
+// console.log(flag)
 
 // 3) Speacial Type
 // Special type wo hute ha ja ham typescript per depend kar deta ha ka agar hamare type number huwe to 
@@ -33,6 +33,11 @@ console.log(flag)
 // Example:
 // var arr:[number,string,boolean] = [1,'ali',true]
 // console.log(arr)
+
+// var arr1: readonly [number,string,boolean] = [1,'bilal',true];
+// arr1.push('khan')
+// console.log(arr1)
+
 
 // 2) Enum 
 // Enum Set Speacific keys in constant
@@ -50,11 +55,41 @@ console.log(flag)
 
 // 2) String 
 enum obj2 {
-    sname='name',
+    sname='byd',
     sage='age',
     sroll='roll'
 }
-console.log(obj2)
+console.log(obj2.sname)
+
+// --- Object two main type
+// 1) type alias
+// 2) interface
+
+// type alias
+// type bname = string;
+// type bmodel = number
+// type bactive = boolean
+
+// interface
+interface bikeType {
+    bname:string,
+    bmodal:number,
+    bactive:boolean,
+    bnumber:number
+}
+const bike:bikeType = {
+    bname:'unique',
+    bmodal:2025,
+    bactive:true,
+    bnumber:123
+}
+
+console.log("bilke=>",bike)
+
+
+
+
+
 
 
 
